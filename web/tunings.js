ef38a2a3367ea499a3bad42e9eaca5322b656135
Roy Wright <roy@wright.org>
2020-10-18 11:38:25 -0500

    added more tunings

--- web/tunings.js
+++ web/tunings.js
@@ -2,14 +2,34 @@ var tunings = {
     "Standard": ["E2", "A2", "D3", "G3", "B3", "E4"],
     "Drop-D": ["D2", "A2", "D3", "G3", "B3", "E4"],
     "Nashville": ["E3", "A3", "D4", "G4", "B3", "E4"],
-    "Open A": ["E", "A", "C♯", "E", "A", "E"],
-    "Open A Slide": ["E", "A", "E", "A", "C♯", "E"],
-    "Open A Alternative": ["E", "A", "E", "A", "E", "C♯"]
+    "Open A (A,C♯,E)": ["E", "A", "C♯", "E", "A", "E"],
+    "Open A (A,C♯,E) Slide": ["E", "A", "E", "A", "C♯", "E"],
+    "Open A (A,C♯,E) Alternative": ["E", "A", "E", "A", "E", "C♯"],
+    "Open A	(A,C♯,E) Repetive":	["A", "C♯", "E", "A", "C♯", "E"],
+    "Open A (A,C♯,E) Overtones": ["A", "A", "E", "A", "C♯", "E"],
+    "Open B	(B,D♯,F♯) Repetive":	["B", "D♯", "F♯", "B", "D♯", "F♯"],
+    "Open B	(B,D♯,F♯) Overtones": ["B", "B", "F♯", "B", "D♯", "F♯"],
+    "Open B	(B,D♯,F♯)": ["B", "F♯", "B", "F♯", "B", "D♯"],
+    "Open C	(C,E,G)": ["C", "G", "C", "G", "C", "E"],
+    "Open C	(C,E,G) Repetive": ["C", "E", "G", "C", "E", "G"],
+    "Open C	(C,E,G) Overtones": ["C", "C", "G", "C", "E", "G"],
+    "Open D	(D,F♯,A)": ["D", "A", "D", "F♯", "A", "D"],
+    "Open D	(D,F♯,A) Repetive": ["D", "F♯", "A", "D", "F♯", "A"],
+    "Open D	(D,F♯,A) Overtones": ["D", "D", "A", "D", "F♯", "A"],
+    "Open E	(E,G♯,B)": ["E", "G♯", "B", "E", "G♯", "B"],
+    "Open E	(E,G♯,B)": ["E", "E", "B", "E", "G♯", "B"],
+    "Open E	(E,G♯,B)": ["E", "B", "E", "G♯", "B", "E"],
+    "Open F	(F,A,C)": ["F", "A", "C", "F", "A", "C"],
+    "Open F	(F,A,C)": ["F", "F", "C", "F", "A", "C"],
+    "Open F	(F,A,C)": ["C", "F", "C", "F", "A", "F"],
+    "Open G	(G,B,D)": ["G", "B", "D", "G", "B", "D"],
+    "Open G	(G,B,D)": ["G", "G", "D", "G", "B", "D"],
+    "Open G	(G,B,D)": ["D", "G", "D", "G", "B", "D"]
 };
 
-var tunings_data = [];
-for (var k in tunings) {
-    var value = k;
+let tunings_data = [];
+for (let k in tunings) {
+    let value = k;
     value += " - ";
     value += tunings[k];
     tunings_data.push(value);
