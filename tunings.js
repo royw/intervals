d86e543750346bb2149b4d8f68d9de0ff5ac7ff9
Roy Wright <roy_wright@trendmicro.com>
2020-10-25 01:54:25 -0500

    corrected tuning persistence

--- tunings.js
+++ tunings.js
@@ -5,26 +5,26 @@ var tunings = {
     "6 - Open A (A,C♯,E)": ["E2", "A2", "C♯3", "E3", "A3", "E4"],
     "6 - Open A (A,C♯,E) Slide": ["E2", "A2", "E3", "A3", "C♯4", "E4"],
     "6 - Open A (A,C♯,E) Alternative": ["E2", "A2", "E3", "A3", "E4", "C♯4"],
-    "6 - Open A	(A,C♯,E) Repetitive":	["A2", "C♯3", "E3", "A3", "C♯4", "E4"],
+    "6 - Open A (A,C♯,E) Repetitive": ["A2", "C♯3", "E3", "A3", "C♯4", "E4"],
     "6 - Open A (A,C♯,E) Overtones": ["A", "A", "E", "A", "C♯", "E"],
-    "6 - Open B	(B,D♯,F♯) Repetitive":	["B", "D♯", "F♯", "B", "D♯", "F♯"],
-    "6 - Open B	(B,D♯,F♯) Overtones": ["B", "B", "F♯", "B", "D♯", "F♯"],
-    "6 - Open B	(B,D♯,F♯)": ["B", "F♯", "B", "F♯", "B", "D♯"],
-    "6 - Open C	(C,E,G)": ["C", "G", "C", "G", "C", "E"],
-    "6 - Open C	(C,E,G) Repetitive": ["C", "E", "G", "C", "E", "G"],
-    "6 - Open C	(C,E,G) Overtones": ["C", "C", "G", "C", "E", "G"],
-    "6 - Open D	(D,F♯,A)": ["D", "A", "D", "F♯", "A", "D"],
-    "6 - Open D	(D,F♯,A) Repetitive": ["D", "F♯", "A", "D", "F♯", "A"],
-    "6 - Open D	(D,F♯,A) Overtones": ["D", "D", "A", "D", "F♯", "A"],
-    "6 - Open E	(E,G♯,B)": ["E", "B", "E", "G♯", "B", "E"],
-    "6 - Open E	(E,G♯,B) Repetitive": ["E", "G♯", "B", "E", "G♯", "B"],
-    "6 - Open E	(E,G♯,B) Overtones": ["E", "E", "B", "E", "G♯", "B"],
-    "6 - Open F	(F,A,C)": ["C", "F", "C", "F", "A", "F"],
-    "6 - Open F	(F,A,C) Repetitive": ["F", "A", "C", "F", "A", "C"],
-    "6 - Open F	(F,A,C) Overtones": ["F", "F", "C", "F", "A", "C"],
-    "6 - Open G	(G,B,D)": ["D", "G", "D", "G", "B", "D"],
-    "6 - Open G	(G,B,D) Repetitive": ["G", "B", "D", "G", "B", "D"],
-    "6 - Open G	(G,B,D) Overtones": ["G", "G", "D", "G", "B", "D"],
+    "6 - Open B (B,D♯,F♯) Repetitive": ["B", "D♯", "F♯", "B", "D♯", "F♯"],
+    "6 - Open B (B,D♯,F♯) Overtones": ["B", "B", "F♯", "B", "D♯", "F♯"],
+    "6 - Open B (B,D♯,F♯)": ["B", "F♯", "B", "F♯", "B", "D♯"],
+    "6 - Open C (C,E,G)": ["C", "G", "C", "G", "C", "E"],
+    "6 - Open C (C,E,G) Repetitive": ["C", "E", "G", "C", "E", "G"],
+    "6 - Open C (C,E,G) Overtones": ["C", "C", "G", "C", "E", "G"],
+    "6 - Open D (D,F♯,A)": ["D", "A", "D", "F♯", "A", "D"],
+    "6 - Open D (D,F♯,A) Repetitive": ["D", "F♯", "A", "D", "F♯", "A"],
+    "6 - Open D (D,F♯,A) Overtones": ["D", "D", "A", "D", "F♯", "A"],
+    "6 - Open E (E,G♯,B)": ["E", "B", "E", "G♯", "B", "E"],
+    "6 - Open E (E,G♯,B) Repetitive": ["E", "G♯", "B", "E", "G♯", "B"],
+    "6 - Open E (E,G♯,B) Overtones": ["E", "E", "B", "E", "G♯", "B"],
+    "6 - Open F (F,A,C)": ["C", "F", "C", "F", "A", "F"],
+    "6 - Open F (F,A,C) Repetitive": ["F", "A", "C", "F", "A", "C"],
+    "6 - Open F (F,A,C) Overtones": ["F", "F", "C", "F", "A", "C"],
+    "6 - Open G (G,B,D)": ["D", "G", "D", "G", "B", "D"],
+    "6 - Open G (G,B,D) Repetitive": ["G", "B", "D", "G", "B", "D"],
+    "6 - Open G (G,B,D) Overtones": ["G", "G", "D", "G", "B", "D"],
     "4 - Reentrant ukulele": ["G4", "C4", "E4", "A4"],
     "4 - Canadian Ukulele": ["A4", "D4", "F♯4", "B4"],
     "4 - Linear  Ukulele alt": ["G3", "C4", "E4", "A4"],
@@ -51,12 +51,12 @@ var tunings = {
     "3 - Open G:": ["G3", "B3", "D4"],
     "3 - Fifths:": ["E3", "B3", "F4#"],
     "7 - Standard": ["B1", "E2", "A2", "D3", "G3", "B3", "E4"],
-    "7 - Russion": ["D2", "G2", "B2", "D3", "G3", "B3", "D4"],
+    "7 - Russian": ["D2", "G2", "B2", "D3", "G3", "B3", "D4"],
     "7 - Classical": ["C2", "G2", "B2", "D3", "G3", "B3", "D4"],
     "7 - Big Guitar": ["G2", "C3", "E3", "G3", "C4", "E4", "G4"],
     "7 - 1/3rds": ["F2", "B♭2", "D3", "F3", "B♭3", "D4", "F4"],
-    "7 - Brazilian": ["C2 E2 A2 D3 G3 B3"],
-    "7 - Brazilian 2": ["B1 E2 A2 D3 G3 B3 E4"],
+    "7 - Brazilian": ["C2", "E2", "A2", "D3", "G3", "B3", "E4"],
+    "7 - Brazilian 2": ["B1", "E2", "A2", "D3", "G3", "B3", "E4"],
     "7 - alt 1": ["E2", "A2", "B3", "D3", "G3", "B3", "D4"],
     "7 - alt 2": ["E2", "G2", "B2", "D3", "G3", "B3", "D4"],
     "7 - alt 3": ["D2", "G2", "C3", "D3", "G3", "B♭3", "D4"],
@@ -72,7 +72,7 @@ var tunings = {
 let tunings_data = [];
 for (let k in tunings) {
     let value = k;
-    if (tunings[k].length > 0) {
+    if (0 < tunings[k].length) {
         value += " - ";
         value += tunings[k];
         tunings_data.push(value);
@@ -85,7 +85,6 @@ function onlyUnique(value, index, self) {
 
 let string_data = [];
 for (let k in tunings) {
-    let strings = [];
     let value = k.split(" - ")[0];
     string_data.push(value);
 }
