f1df6d7f67cb34fc727dc03566eb127de16427ba
Roy Wright <roy@wright.org>
2020-10-25 21:50:01 -0500

    add custom tunings

--- menu.js
+++ menu.js
@@ -1,32 +1,114 @@
 var menu_data = [
-    {
-        "label": "Frets",
-        "default": 24,
-        "values": [12, 24]
-    },
-    {
-        "label": "Strings",
-        "default": "6",
-        "values": string_data
-    },
-    {
-        "label": "Tuning",
-        "default": "6 - Standard",
-        "values": tunings_data
-    },
-    {
-        "label": "Key",
-        "default": "C",
-        "values": keys_data
-    },
-    // {
-    //     "label": "Interval",
-    //     "default": "I",
-    //     "values": interval_data
-    // },
-    {
-        "label": "Mode / Scale",
-        "default": "Ionian mode or major scale",
-        "values": modes_data
-    }
-];
+        {
+            "id": "menu1",
+            "contains": [
+                {
+                    "label": "Frets",
+                    "default": 24,
+                    "values": [12, 24]
+                },
+                {
+                    "label": "Strings",
+                    "default": "6",
+                    "values": string_data
+                },
+                {
+                    "label": "Tuning",
+                    "default": "6 - Standard",
+                    "values": tunings_data
+                },
+                {
+                    "label": "Key",
+                    "default": "C",
+                    "values": keys_data
+                },
+                // {
+                //     "label": "Interval",
+                //     "default": "I",
+                //     "values": interval_data
+                // },
+                {
+                    "label": "Mode / Scale",
+                    "default": "Ionian mode or major scale",
+                    "values": modes_data
+                },
+            ]
+        },
+        {
+            "id": "menu2",
+            "contains": [
+                {
+                    "label":
+                        "String 8",
+                    "default":
+                        "C",
+                    "values":
+                    keys_data
+                }
+                ,
+                {
+                    "label":
+                        "String 7",
+                    "default":
+                        "C",
+                    "values":
+                    keys_data
+                }
+                ,
+                {
+                    "label":
+                        "String 6",
+                    "default":
+                        "C",
+                    "values":
+                    keys_data
+                }
+                ,
+                {
+                    "label":
+                        "String 5",
+                    "default":
+                        "C",
+                    "values":
+                    keys_data
+                }
+                ,
+                {
+                    "label":
+                        "String 4",
+                    "default":
+                        "C",
+                    "values":
+                    keys_data
+                }
+                ,
+                {
+                    "label":
+                        "String 3",
+                    "default":
+                        "C",
+                    "values":
+                    keys_data
+                }
+                ,
+                {
+                    "label":
+                        "String 2",
+                    "default":
+                        "C",
+                    "values":
+                    keys_data
+                }
+                ,
+                {
+                    "label":
+                        "String 1",
+                    "default":
+                        "C",
+                    "values":
+                    keys_data
+                }
+            ]
+        }
+    ]
+;
